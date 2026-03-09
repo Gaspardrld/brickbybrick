67e43fc300912a9c361ca8bb4f462bb46c00d9fb
moulatarte <antoine.devilez1@gmail.com>
2026-03-09 09:17:38 +0100

    feat(paddle): implémentation initiale du module Paddle

--- paddle.cc
+++ paddle.cc
@@ -1,1 +1,11 @@
 #include "paddle.h"
+
+Circle Paddle::get_circle() const {
+    return form;
+}
+
+bool Paddle::validate_paddle() const {
+    if (form.radius <= 0.0) {
+        return false;
+    }
+    if (not(point_in_square(form.center, {{arena_size/2, arena_size/2}, arena_size}, false, true))
--- paddle.h
+++ paddle.h
@@ -5,7 +5,10 @@ using namespace std;
 
 
 class Paddle {
-    private :
-        Circle form;
-    public :
+private :
+    Circle form;
+public :
+    Paddle(double x, double y, double radius) : form({{x,y}, radius}) {}
+    Circle get_circle() const;
+    bool validate_paddle() const;
 };
