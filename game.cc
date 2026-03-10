753de2389ecac2d096c0d10031565ac40f49a696
Gaspardrld <126902802+Gaspardrld@users.noreply.github.com>
2026-03-10 12:30:31 +0100

    fix(game): désactivation de la tolérance pour la collision brick-brick

--- game.cc
+++ game.cc
@@ -126,7 +126,7 @@ namespace {
                 }
 
                 for (size_t i = 0; i < bricks.size(); ++i) {
-                    if (squares_intersect(new_brick->get_form(), bricks[i]->get_form())) {
+                    if (squares_intersect(new_brick->get_form(), bricks[i]->get_form(), false)) {
                         cout << message::collision_bricks(nb_bricks_read, i);
                         delete new_brick;
                         return false;
