3f7bf96e9cc556b28c24b8792c5fafc1f06799f8
Gaspardrld <126902802+Gaspardrld@users.noreply.github.com>
2026-03-10 11:32:30 +0100

    feat(project): ajout de la lecture du fichier de jeu via argument CLI

--- project.cc
+++ project.cc
@@ -1,6 +1,17 @@
 #include <iostream>
 #include "game.h"
 
-int main() {
+using namespace std;
 
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        cout << "Usage: ./project fichier.txt" << endl;
+        return 1;
+    }
+    
+    if (game::read(argv[1])) {
+        return 0;
+    } else {
+        return 1;
+    }
 }
