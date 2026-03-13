1cefb263fd8f6751b49cd50f9e0d12b10a622c35
Gaspardrld <126902802+Gaspardrld@users.noreply.github.com>
2026-03-13 19:51:47 +0100

    GRAHHHHHH

--- project.cc
+++ project.cc
@@ -4,11 +4,12 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+    game game;
     if (argc != 2) {
         return 1;
     }
     
-    if (game::read(argv[1])) {
+    if (game.read(argv[1])) {
         return 0;
     } else {
         return 1;
