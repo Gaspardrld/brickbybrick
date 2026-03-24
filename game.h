5f42e55da2aaaaec6944c8e5fa06deaa421d9597
Gaspardrld <126902802+Gaspardrld@users.noreply.github.com>
2026-03-24 12:25:14 +0100

    finish decode line reduit

--- game.h
+++ game.h
@@ -22,15 +22,13 @@ class Game {
             FINISH,
         };
         bool decode_line(const std::string& line);
-        bool verif_score(istringstream& iss, double& total_score);
-        bool verif_lives(istringstream& iss, int& nb_lives);
-        bool verif_paddle(istringstream& iss, Paddle& paddle);
-        bool verif_nb_bricks(istringstream& iss, int& nb_bricks);
-        bool verif_brick(istringstream& iss, int& nb_bricks_read, 
-                                vector<Brick*>& bricks, Paddle& paddle);
-        bool verif_nb_balls(istringstream& iss, int& nb_balls);
-        bool verif_balls(istringstream& iss, int& nb_balls_read, vector<Ball>& balls
-                                        ,vector<Brick*>& bricks, Paddle& paddle);
+        bool verif_score(istringstream& iss);
+        bool verif_lives(istringstream& iss);
+        bool verif_paddle(istringstream& iss);
+        bool verif_nb_bricks(istringstream& iss);
+        bool verif_brick(istringstream& iss);
+        bool verif_nb_balls(istringstream& iss);
+        bool verif_balls(istringstream& iss);
 
         double total_score;
         int nb_lives;
