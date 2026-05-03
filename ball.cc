6ef6442d89b2887b1542c65579710e286bcbca4e
moulatarte <antoine.devilez1@gmail.com>
2026-05-03 15:59:14 +0200

    feat: implémentation des collisions balle-mur et balle-paddle avec physique élastique

--- ball.cc
+++ ball.cc
@@ -47,3 +47,6 @@ void Ball::move() {
     form.center.x += delta.x;
     form.center.y += delta.y;
 }
+
+void Ball::undo_move() { form.center.x -= delta.x; form.center.y -= delta.y; }
+void Ball::set_delta(Point d) { delta = d; }
